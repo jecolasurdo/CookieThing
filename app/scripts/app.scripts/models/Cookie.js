e268703c21dad82d8e357a175369ea795f87ec61
Chris Francisco <cfranciscodv@gmail.com>
2015-08-08 21:52:56 -0700

    adding Cookie model

---
+++ app/scripts/app.scripts/models/Cookie.js
@@ -0,0 +1,14 @@
+﻿var CookieThing = {
+    Cookie: {
+        name: "",
+        value: "",
+        domain: "",
+        hostOnly: false,
+        path: "",
+        secure: false,
+        httpOnly: false,
+        session: false,
+        expirationDate: 0,
+        storeId: ""
+    }
+}
