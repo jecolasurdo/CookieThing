aaad36e4f6ab1572b7aa9a3e1c8f7075a98a3ba9
Joe Colasurdo <jcolasurdo@nationalresearch.com>
2015-08-08 22:51:54 -0700

    New: Created observable list of domains.

--- app/scripts/app.scripts/viewmodels/CookieThingViewModel.js
+++ app/scripts/app.scripts/viewmodels/CookieThingViewModel.js
@@ -1,9 +1,17 @@
 ﻿/// <reference path="../../knockout-3.3.0.js" />d
 /// <reference path="../../jquery-2.1.4.js" />
+/// <reference path="../../underscore.js" />
+
 
 var CookieThingViewModel = function (cookies) {
     this.cookies = ko.observableArray(cookies);
-    this.domains = {};
+    this.domains = ko.computed(function () {
+        return _.sortBy(
+                   _.uniq(
+                       _.map(this.cookies(), function (cookie) {
+                           return cookie.domain;
+                       })), function (domain) { return domain; });
+        }, this);
 }
 
 ko.applyBindings(
