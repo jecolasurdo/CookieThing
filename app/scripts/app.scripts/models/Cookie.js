6779617387553b21ebd532de5802dd2c1b1f0c0d
Joe Colasurdo <jcolasurdo@nationalresearch.com>
2015-08-10 10:51:51 -0700

    Fix: Revised the viewModel to be more knockoutish and encorporated the cookie CookieThing.Cookie object for null handling.

--- app/scripts/app.scripts/models/Cookie.js
+++ app/scripts/app.scripts/models/Cookie.js
@@ -1,5 +1,5 @@
 ﻿var CookieThing = {
-    Cookie: {
+    DefaultCookie: {
         name: "",
         value: "",
         domain: "",
@@ -10,5 +10,22 @@
         session: false,
         expirationDate: 0,
         storeId: ""
+    },
+
+    Cookie: function (rawCookie) {
+        if (rawCookie) {
+            this.name = rawCookie.name;
+            this.value = rawCookie.value;
+            this.domain = rawCookie.domain;
+            this.hostOnly = rawCookie.hostOnly;
+            this.path = rawCookie.path;
+            this.secure = rawCookie.secure;
+            this.httpOnly = rawCookie.httpOnly;
+            this.session = rawCookie.session;
+            this.expirationDate = rawCookie.expirationDate;
+            this.storeId = rawCookie.storeId;
+        } else {
+            return CookieThing.DefaultCookie;
+        }
     }
 }
--- app/scripts/app.scripts/viewmodels/CookieThingViewModel.js
+++ app/scripts/app.scripts/viewmodels/CookieThingViewModel.js
@@ -1,52 +1,53 @@
 ﻿/// <reference path="../../knockout-3.3.0.js" />d
 /// <reference path="../../jquery-2.1.4.js" />
 /// <reference path="../../underscore.js" />
+/// <reference path="../models/Cookie.js" />
 
 var CookieThingViewModel = function () {
-    this.cookies = ko.observableArray();
-    this.cookieNamesForSelectedDomain = ko.observable();
-    this.selectedCookie = ko.observable();
-    this.selectedDomain = ko.observable();
+    this.cookies = ko.observableArray([]);
+    this.selectedCookieName = ko.observable('');
+    this.selectedDomain = ko.observable('');
 
-    this.domains = ko.computed(function () {
-        return _.sortBy(
-                   _.uniq(
-                       _.map(this.cookies()[0], function (cookie) {
-                           return cookie.domain;
-                       })), function (domain) { return domain; });
+    this.domains = ko.computed(
+        function () {
+            return _.sortBy(
+                       _.uniq(
+                           _.map(this.cookies()[0]
+                                , function (cookie) { return cookie.domain; }
+                                , this)
+                          , false
+                          , function (a) { return a }
+                          , this)
+                     , function (domain) { return domain; }
+                     , this);
+        }
+        , this);
+
+    this.cookieNamesForSelectedDomain = ko.computed(
+        function () {
+            return _.map(
+                       _.filter(this.cookies()[0]
+                           , function (cookie) { return cookie.domain === this.selectedDomain(); }
+                           , this)
+                       , function (cookie) { return cookie.name; }
+                       , this);
+        }, this);
+
+    this.selectedRawCookie = ko.computed(function () {
+        return _.filter(this.cookies()[0]
+             , function (cookie) {
+                 return cookie.name === this.selectedCookieName()
+                     && cookie.domain === this.selectedDomain();
+             }
+             , this)[0];
     }, this);
 
-
-    this.getCookiesNamesForDomain = function (domain) {
-        this.selectedDomain(domain);
-        this.cookieNamesForSelectedDomain(
-            _.map(
-                   _.filter(this.cookies()[0], function (cookie) {
-                       return cookie.domain === domain;
-                   }),
-                   function (cookie) {
-                       return cookie.name;
-                   })
-            );
-    }
-
-    this.setSelectedCookie = function (cookieName) {
-        selectedDomain = this.selectedDomain();
-        this.selectedCookie(
-            _.first(
-                _.filter(this.cookies()[0],
-                    function (cookie) {
-                        return cookie.name === cookieName
-                            && cookie.domain === selectedDomain;
-                    }
-                )
-            )
-        );
-    }
+    this.selectedCookie = ko.computed(function () {
+        return new CookieThing.Cookie(this.selectedRawCookie());
+    }, this);
 
 }
 
 var ctvm = new CookieThingViewModel();
 chrome.cookies.getAll({}, function (cookie) { ctvm.cookies.push(cookie); });
-
 ko.applyBindings(ctvm);
