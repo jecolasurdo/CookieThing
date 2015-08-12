816ac424da17cf8c41f1d0d74db5bac5ed84c36f
Joe Colasurdo <jcolasurdo@nationalresearch.com>
2015-08-11 17:36:07 -0700

    New: Refactored CookieThingViewModel into ViewModel namespace.

--- app/scripts/CookieThing/ViewModels/CookieThingViewModel.js
+++ app/scripts/CookieThing/ViewModels/CookieThingViewModel.js
@@ -3,51 +3,57 @@
 /// <reference path="../../underscore.js" />
 /// <reference path="../Models/Cookie.js" />
 
-var CookieThingViewModel = function () {
-    this.cookies = ko.observableArray([]);
-    this.selectedCookieName = ko.observable('');
-    this.selectedDomain = ko.observable('');
+var CookieThing;
+(function (CookieThing) {
+    (function (ViewModels) {
+        ViewModels.CookieThingViewModel = function () {
+            this.cookies = ko.observableArray([]);
+            this.selectedCookieName = ko.observable('');
+            this.selectedDomain = ko.observable('');
 
-    this.domains = ko.computed(
-        function () {
-            return _.sortBy(
-                       _.uniq(
-                           _.map(this.cookies()[0]
-                                , function (cookie) { return cookie.domain; }
-                                , this)
-                          , false
-                          , function (a) { return a }
-                          , this)
-                     , function (domain) { return domain; }
-                     , this);
+            this.domains = ko.computed(
+                function () {
+                    return _.sortBy(
+                               _.uniq(
+                                   _.map(this.cookies()[0]
+                                        , function (cookie) { return cookie.domain; }
+                                        , this)
+                                  , false
+                                  , function (a) { return a }
+                                  , this)
+                             , function (domain) { return domain; }
+                             , this);
+                }
+                , this);
+
+            this.cookieNamesForSelectedDomain = ko.computed(
+                function () {
+                    return _.map(
+                               _.filter(this.cookies()[0]
+                                   , function (cookie) { return cookie.domain === this.selectedDomain(); }
+                                   , this)
+                               , function (cookie) { return cookie.name; }
+                               , this);
+                }, this);
+
+            this.selectedRawCookie = ko.computed(function () {
+                return _.filter(this.cookies()[0]
+                     , function (cookie) {
+                         return cookie.name === this.selectedCookieName()
+                             && cookie.domain === this.selectedDomain();
+                     }
+                     , this)[0];
+            }, this);
+
+            this.selectedCookie = ko.computed(function () {
+                return new CookieThing.Models.Cookie(this.selectedRawCookie());
+            }, this);
+
         }
-        , this);
+    })(CookieThing.ViewModels || (CookieThing.ViewModels = {}))
+    var ViewModels = CookieThing.ViewModels;
+})(CookieThing || (CookieThing = {}));
 
-    this.cookieNamesForSelectedDomain = ko.computed(
-        function () {
-            return _.map(
-                       _.filter(this.cookies()[0]
-                           , function (cookie) { return cookie.domain === this.selectedDomain(); }
-                           , this)
-                       , function (cookie) { return cookie.name; }
-                       , this);
-        }, this);
-
-    this.selectedRawCookie = ko.computed(function () {
-        return _.filter(this.cookies()[0]
-             , function (cookie) {
-                 return cookie.name === this.selectedCookieName()
-                     && cookie.domain === this.selectedDomain();
-             }
-             , this)[0];
-    }, this);
-
-    this.selectedCookie = ko.computed(function () {
-        return new CookieThing.Models.Cookie(this.selectedRawCookie());
-    }, this);
-
-}
-
-var ctvm = new CookieThingViewModel();
+var ctvm = new CookieThing.ViewModels.CookieThingViewModel();
 chrome.cookies.getAll({}, function (cookie) { ctvm.cookies.push(cookie); });
 ko.applyBindings(ctvm);
